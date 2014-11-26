8e4c4f0ac8536752833757e3f6bc38460edf4326
Steampilot <github@steampilot.ch>
2014-11-26 18:20:06 +0100

    Refactoring all functions to comply with the Object Oriented Programming

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,14 +29,52 @@ app.Index = function (config) {
 	 * @returns {undefined}
 	 */
 	this.init = function () {
-		// init events
-		$('#myelement').on('click', this.myelement_onClick);
+
+		$('#page_control').hide();
+		$('#profession').hide();
+		$('#school_class').hide();
+		$('#not_found').hide();
+		$('#board').hide();
+
+		$('#datepicker').datepicker({
+			autoclose: true,
+			weekStart: 1,
+			todayBtn: 'linked',
+			calendarWeeks: true,
+			selectWeek: true,
+			todayHighLight: true,
+			format: 'yyyy-mm-dd'
+		});
+
+		$('#datepicker').datepicker().val($this.getCurrentDate());
+		$('#datepicker').datepicker().on('hide', this.datepicker_onHide);
+		$('#profession').on('change', this.profession_onChange);
+		$('#school_class').on('change', this.school_class_onChange);
+		$('#previous').on('click', this.previous_onClick);
+		$('#next').on('click', this.next_onClick);
+		$('#datepicker').on('keydown', this.datepicker_onKeyDown);
 
 	};
 
 	this.load = function () {
 		// rpc request
-// load form data
+		// load form data
+		// when the page has finished loading
+		$this.getProfession();
+
+
+		var professionId = $d.getCookie("professionId", 0);
+		if (professionId > 0) {
+			$('#profession').val(professionId);
+			//$('#profession').trigger('change');
+			var classId = $d.getCookie("classId", 0);
+			if (classId > 0) {
+				$('#school_class').val(classId);
+				$('#school_class').trigger('change');
+			}
+		}
+
+
 	};
 
 	/**
@@ -50,60 +88,191 @@ app.Index = function (config) {
 		// $this.config
 	};
 
-
-
-	this.init();
-
-};
-
-
-$(function () {
-	var page = new app.Index();
-	page.load();
-});
-
-
-paginate = true;
-$(function () {
-
+	this.getCurrentDate = function(){
+		var d = new Date();
+		var n = d.toISOString();
+		var result = n.substring(0,10);
+		return result;
+	};
+	/**
+	 * Gets the week numbre by a given date object
+	 * @param d {Date}
+	 * @returns {number}
+	 */
+	this.getWeek = function (d) {
+		d = new Date(+d);
+		d.setHours(0, 0, 0, 0);
+		d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+		var yearStart = new Date(d.getFullYear(), 0, 1);
+		var weekNr = Math.ceil(( ( (d - yearStart) / 86400000) + 1) / 7);
+		var weekYear = weekNr+'-'+yearStart;
+		var result = {
+			weekYear: weekNr,
+			weekNr: weekNr,
+			yearStart: yearStart
+		};
+ 		return result;
+	};
 
 	/**
-	 * The date object to work with, initialized as now
-	 * @type {Date}
+	 * gets the name and the id of a profession via JSON response
 	 */
-	var d = new Date();
+	this.getProfession = function () {
+		$.ajax({
+			type: "POST",
+			url: "http://home.gibm.ch/interfaces/133/berufe.php"
+		}).done(function (response) {
+			$this.displayProfession(response);
 
+			$('#profession').show('slow');
+		});
+	};
+
+	this.getSchoolClassByProfessionId = function (professionId) {
+		$.ajax({
+			type: 'POST',
+			url: 'http://home.gibm.ch/interfaces/133/klassen.php?beruf_id=' + professionId
+		}).done(function (response) {
+			$this.displaySchoolClass(response);
+
+			$('#school_class').show('slow');
+		});
+	};
+
+	this.getBoard = function () {
+		debugger;
+		var classId = $('#school_class').val();
+		var selectedDate = $('#datepicker').val();
+		console.log(selectedDate);
+		var year = $this.getWeek(selectedDate);
+		$('#page_control').fadeIn('slow');
+		$('#board').hide('slow');
+		$('#not_found').hide('slow');
+		var week_year = 'Aktuell';
+		var url = 'http://home.gibm.ch/interfaces/133/tafel.php?klasse_id=' + classId;
+		if (typeof week === 'undefined' && typeof year === 'undefined') {
+			url += '&woche=' + week + '-' + year;
+			week_year = week + '-' + year;
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: url
+		}).done(function (response) {
+			$this.displayBoard(response, week_year);
+		});
+	};
 	/**
-	 * The year of a date
-	 * @type {number}
+	 * Builds the drop down options for selecting a profession
+	 * @param profession
 	 */
-	var year = d.getFullYear();
+	this.displayProfession = function (profession) {
+		var options = '<option value="0">** Bitte wählen Sie einen Beruf aus</option>';
+		for (var i in profession) {
+			var row = profession[i];
+			options +=
+				'<option value="' +
+				gh(row.beruf_id) + '">' +
+				gh(row.beruf_name) +
+				'</option>';
+		}
+		$('#profession').html(options);
+	};
 
-	/**
-	 * The number of a week in a year
-	 * @type {number}
-	 */
-	var week = getWeekNumber(d);
+	this.displaySchoolClass = function (schoolClass) {
+		var options = '<option value="0">** Bitte wählen Sie eine Klasse aus</option>';
+		for (var i in schoolClass) {
+			var row = schoolClass[i];
+			options +=
+				'<option value="' +
+				gh(row.klasse_id) + '">' +
+				gh(row.klasse_name) + ' - ' +
+				gh(row.klasse_longname) +
+				'</option>';
+		}
+		$('#school_class').html(options);
+	};
+
+	this.prepareBoard = function (rows) {
+		var result = [];
+		for (var i in rows) {
+			var row = rows[i];
+			var weekday = row.tafel_wochentag;
+			if (!(weekday in result)) {
+				result[weekday] = [];
+			}
+			result[weekday].push(row);
+		}
+		console.log(result);
+		return result;
+
+	};
+
+	this.displayBoard = function (board, week_year) {
+		console.log(week_year);
+		$('#week_year').html('Woche: ' + gh(week_year));
+
+		$('#lecture_board').html('');
+		var date = null;
+
+		var result = $this.prepareBoard(board);
+		for (var weekday in result) {
+			var lecture = result[weekday];
+			// creating jquery dom object of the cloned html template
+			var tpl = $($('#board_content').html());
+
+			var tbody = tpl.find('tbody');
+			var select = $($('#select_date_content').html());
+			tpl.find('#week_day').html(gh($this.getWeekDayName(weekday)));
+			tpl.find('#select_date').append(select);
+			console.log(tbody);
+			var rows = 'Lectures: ';
+			console.log('lecture[0] ');
+			for (var i in lecture) {
+				rows += '<tr>';
+				var row = lecture[i];
+
+				tpl.find('#date').html(gh(row.tafel_datum));
+				date = row.tafel_datum;
+				rows += '<td>' + gh(row.tafel_von) + '</td>';
+				rows += '<td>' + gh(row.tafel_bis) + '</td>';
+				rows += '<td>' + gh(row.tafel_raum) + '</td>';
+				rows += '<td>' + gh(row.tafel_fach) + '</td>';
+				rows += '<td>' + gh(row.tafel_longfach) + '</td>';
+				rows += '<td>' + gh(row.tafel_lehrer) + '</td>';
+				rows += '<td>' + gh(row.tafel_kommentar) + '</td>';
+				rows += '</tr>';
+			}
+			tbody.html(rows);
+			$('#lecture_board').append(tpl);
+		}
+
+		console.log('board: ' + board);
+		if (date != null) {
+			$('#board').show('slow');
+			$('#not_found').hide('slow');
+			date = null;
+		} else {
+			$('#board').hide('slow');
+			$('#not_found').show('slow');
+		}
+	};
 
 
-	//var classId = $d.getCookie('classId', null);
-	/**
-	 * Init the date input field with a placeholder
-	 * @function $('#datepicker') The date input field
-	 */
-	$('#datepicker').attr('placeholder', week + '-' + year);
-	$('#page_control').hide();
-	$('#profession').hide();
-	$('#school_class').hide();
-	$('#not_found').hide();
-	$('#board').hide();
+	this.getWeekDayName = function (id) {
+		var weekdayName = [
+			'Sontag',
+			'Montag',
+			'Dienstag',
+			'Mittwoch',
+			'Donnerstag',
+			'Freitag',
+			'Samstag'
+		];
+		return weekdayName[id];
+	};
 
-	// when the page has finished loading
-	getProfession();
-
-	// when something has changed
-
-	$('#profession').on('change', function () {
+	this.profession_onChange = function () {
 
 		var professionId = $('#profession').val();
 		$d.setCookie('professionId', professionId);
@@ -115,10 +284,11 @@ $(function () {
 		} else {
 			$('#board').hide('slow');
 			$('#not_found').hide('slow');
-			getSchoolClassByProfessionId(professionId);
+			$this.getSchoolClassByProfessionId(professionId);
 		}
-	});
-	$('#school_class').on('change', function () {
+	};
+
+	this.school_class_onChange = function () {
 		var classId = $('#school_class').val();
 		$d.setCookie('classId', classId);
 		console.log(classId);
@@ -128,258 +298,36 @@ $(function () {
 			$('#not_found').hide('slow');
 		} else {
 			//$d.setCookie('classId', classId);
-			getBoard(classId, week, year);
+			$this.getBoard();
 		}
-	});
-	$('#datepicker').datepicker({
-		autoclose: true,
-		weekStart: 1,
-		todayBtn: 'linked',
-		calendarWeeks: true,
-		selectWeek: true,
-		todayHighLight: true,
-		format: 'yyyy-mm-dd'
-	});
-	$('#previous').on('click', function () {
-		week = week - 1;
-		$('#datepicker').val(week + '-' + year);
-		if (classId != null && week != 0 && year != 0) {
+	};
+	this.previous_onClick = function () {
 
-			getBoard(classId, week, year);
-		}
-	});
-	$('#next').on('click', function () {
-		week = week + 1;
-		$('#datepicker').val(week + '-' + year);
-		if (classId != null && week != 0 && year != 0) {
+			$this.getBoard();
 
-			getBoard(classId, week, year);
-		}
-	});
-	/**
-	 * Prevents typing into the date input field for the common way is to pick the week number by using the datepicker
-	 */
-	$('#datepicker').keydown(function (e) {
+	};
+
+	this.next_onClick = function () {
+
+			$this.getBoard();
+	};
+
+	this.datepicker_onKeyDown = function (e) {
 		e.preventDefault();
 		return false;
-	});
+	};
+	this.datepicker_onHide = function () {
+			$this.getBoard();
 
-	/**
-	 * Calls the datepicker plugin when clicking in the input field
-	 */
-	$('#datepicker').datepicker()
-		.on('hide', function () {
-			var d = new Date($('#datepicker').val());
-			week = getWeekNumber(d);
-			year = d.getFullYear();
-			$('#datepicker').val(week + '-' + year);
-			if (classId != null && week != 0 && year != 0) {
+	};
 
-				getBoard(classId, week, year);
-			}
 
-		});
-	init();
+	this.init();
+
+};
+
+
+$(function () {
+	var page = new app.Index();
+	page.load();
 });
-
-function init() {
-	var professionId = $d.getCookie("professionId", 0);
-	if (professionId > 0) {
-		$('#profession').val(professionId);
-		//$('#profession').trigger('change');
-		var classId = $d.getCookie("classId", 0);
-		if (classId > 0) {
-			$('#school_class').val(classId);
-			$('#school_class').trigger('change');
-		}
-	}
-
-}
-
-/**
- * Gets the week numbre by a given date object
- * @param d {Date}
- * @returns {number}
- */
-function getWeekNumber(d) {
-	d = new Date(+d);
-	d.setHours(0, 0, 0, 0);
-	d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-	var yearStart = new Date(d.getFullYear(), 0, 1);
-	var weekNr = Math.ceil(( ( (d - yearStart) / 86400000) + 1) / 7)
-	return weekNr;
-}
-
-/**
- * gets the name and the id of a profession via JSON response
- */
-function getProfession() {
-	$.ajax({
-		type: "POST",
-		url: "http://home.gibm.ch/interfaces/133/berufe.php"
-	}).done(function (response) {
-		displayProfession(response);
-
-		$('#profession').show('slow');
-	});
-}
-function getAllSchoolClasses() {
-	$.ajax({
-		type: "POST",
-		url: "http://home.gibm.ch/interfaces/133/klassen.php"
-	}).done(function (response) {
-		displaySchoolClass(response);
-	});
-}
-function getSchoolClassByProfessionId(professionId) {
-	$.ajax({
-		type: 'POST',
-		url: 'http://home.gibm.ch/interfaces/133/klassen.php?beruf_id=' + professionId
-	}).done(function (response) {
-		displaySchoolClass(response);
-
-		$('#school_class').show('slow');
-	});
-}
-function getBoard(classId, week, year) {
-	$('#page_control').fadeIn('slow');
-	$('#board').hide('slow');
-	$('#not_found').hide('slow');
-	console.log('week: ' + week);
-	console.log('year: ' + year);
-	var week_year = 'Aktuel'
-	var url = 'http://home.gibm.ch/interfaces/133/tafel.php?klasse_id=' + classId;
-	if (typeof week === 'undefined' && typeof year === 'undefined') {
-		url += '&woche=' + week + '-' + year;
-		week_year = week + '-' + year;
-	}
-	$.ajax({
-		type: 'POST',
-		url: url
-	}).done(function (response) {
-		response
-
-		displayBoard(response, week_year);
-	});
-}
-/**
- * Builds the drop down options for selecting a profession
- * @param profession
- */
-function displayProfession(profession) {
-	var options = '<option value="0">** Bitte wählen Sie einen Beruf aus</option>';
-	for (var i in profession) {
-		var row = profession[i];
-		options +=
-			'<option value="' +
-			gh(row.beruf_id) + '">' +
-			gh(row.beruf_name) +
-			'</option>';
-	}
-	$('#profession').html(options);
-}
-function displaySchoolClass(schoolClass) {
-	var options = '<option value="0">** Bitte wählen Sie eine Klasse aus</option>';
-	for (var i in schoolClass) {
-		var row = schoolClass[i];
-		options +=
-			'<option value="' +
-			gh(row.klasse_id) + '">' +
-			gh(row.klasse_name) + ' - ' +
-			gh(row.klasse_longname) +
-			'</option>';
-	}
-	$('#school_class').html(options);
-}
-function prepareBoard(rows) {
-	var result = [];
-	var lectures;
-	var row;
-	for (var i in rows) {
-		var row = rows[i];
-		var weekday = row.tafel_wochentag;
-		var date = row.tafel_datum;
-		if (!(weekday in result)) {
-			result[weekday] = [];
-		}
-		result[weekday].push(row);
-	}
-	console.log(result);
-	return result;
-
-}
-function displayBoard(board, week_year) {
-	console.log(week_year);
-	$('#week_year').html('Woche: ' + gh(week_year));
-
-	$('#lecture_board').html('');
-	var date = null;
-
-	var result = prepareBoard(board);
-	for (var weekday in result) {
-		var lecture = result[weekday];
-		// creating jquery dom object of the cloned html template
-		var tpl = $($('#board_content').html());
-
-		var tbody = tpl.find('tbody');
-		var select = $($('#select_date_content').html());
-		tpl.find('#week_day').html(gh(getWeekDayName(weekday)));
-		tpl.find('#select_date').append(select);
-		console.log(tbody);
-		var rows = 'Lectures: ';
-		console.log('lecture[0] ');
-		for (var i in lecture) {
-			rows += '<tr>';
-			var row = lecture[i];
-
-			tpl.find('#date').html(gh(row.tafel_datum));
-			date = row.tafel_datum;
-			rows += '<td>' + gh(row.tafel_von) + '</td>';
-			rows += '<td>' + gh(row.tafel_bis) + '</td>';
-			rows += '<td>' + gh(row.tafel_raum) + '</td>';
-			rows += '<td>' + gh(row.tafel_fach) + '</td>';
-			rows += '<td>' + gh(row.tafel_longfach) + '</td>';
-			rows += '<td>' + gh(row.tafel_lehrer) + '</td>';
-			rows += '<td>' + gh(row.tafel_kommentar) + '</td>';
-			rows += '</tr>';
-		}
-		tbody.html(rows);
-		$('#lecture_board').append(tpl);
-	}
-
-	console.log('board: ' + board);
-	if (date != null) {
-		$('#board').show('slow');
-		$('#not_found').hide('slow');
-		date = null;
-	} else {
-		$('#board').hide('slow');
-		$('#not_found').show('slow');
-	}
-}
-
-/**encodes html conform string
- * @param value
- * @returns {*|jQuery}
- */
-function gh(value) {
-	//create a in-memory div, set it's inner text(which jQuery automatically encodes)
-	//then grab the encoded contents back out.  The div never exists on the page.
-	return $('<div/>').text(value).html();
-}
-function gu(value) {
-	return '<a href="' + value + '">' + value + '</a>';
-}
-
-function getWeekDayName(id) {
-	var weekdayName = [
-		'Sontag',
-		'Montag',
-		'Dienstag',
-		'Mittwoch',
-		'Donnerstag',
-		'Freitag',
-		'Samstag'
-	];
-	return weekdayName[id];
-}
